846fa00566bac7151e136958e0957fdd67b412c6
kameyer226 <61993880+kameyer226@users.noreply.github.com>
2020-05-27 14:13:05 -0500

    Aero daemon3 (#7)
    * uses uart trait added to class Aerotech
    
    * update to readme to help close iss#3
    
    * repeat: uses uart integration, added readme.md change and suggestions
    
    * update to indent on update_state
    
    * update_stage fix, move now works
    
    * added home method for review
    
    * is-homeable update...bug with busy prevents function
    
    * flux state attempt to fix busy
    
    * still in flux due to async await.areadline failure
    
    * flux state continued, updated yaqd-core to fix yaqd-core-python per MR 73 but no apparent changes in behavior
    
    * same as prev but incorporated suggested changes to update_state
    
    * fixed with new yaqd-core package, bytestring update to line 72 (line[0:1])
    
    * removed test script and comments
    
    * comments removed

--- aerotech_101smc2/aerotech_101smc2/_aerotech.py
+++ aerotech_101smc2/aerotech_101smc2/_aerotech.py
@@ -3,18 +3,21 @@ __all__ = ["Aerotech"]
 import asyncio
 from typing import Dict, Any, List
 
-from yaqd_core import ContinuousHardware
+from yaqd_core import ContinuousHardware, aserial
 
 
 class Aerotech(ContinuousHardware):
-    _kind = "Aerotech_101smc2"
-    traits: List[str] = []
-    defaults: Dict[str, Any] = {}
+    _kind = "aerotech-101smc2"
+    traits: List[str] = ["uses-uart","uses-serial","is-homeable"]
+    defaults: Dict[str, Any] = {"baud_rate": 57600}
+    
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
-        # Perform any unique initialization
 
+        self._serial_port = aserial.ASerial(config["serial_port"], config["baud_rate"])
+
+        # perform other setup, possibly including reads and writes
 
     def _load_state(self, state):
         """Load an initial state from a dictionary (typically read from the state.toml file).
@@ -36,21 +39,37 @@ class Aerotech(ContinuousHardware):
         state["value"] = self.value
         return state
 
-
-
-
     def _set_position(self, position):
-        ...
+        self._serial_port.write(f"M {position}\n".encode())
 
+    def close(self):
+        self._serial_port.close()
 
+    def direct_serial_write(self, message):
+        self._busy = True
+        self._serial_port.write(message.encode())
+
+    def home(self):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self._home())
+
+    async def _home(self):
+        self._busy = True
+        # Initiate the home
+        self._serial_port.write(b"H\n")
+        await self._not_busy_sig.wait()
+        self.set_position(self._destination)
+    
     async def update_state(self):
-        """Continually monitor and update the current daemon state."""
-        # If there is no state to monitor continuously, delete this function
         while True:
             # Perform any updates to internal state
-            self._busy = False
-            # There must be at least one `await` in this loop
-            # This one waits for something to trigger the "busy" state
-            # (Setting `self._busy = True)
-            # Otherwise, you can simply `await asyncio.sleep(0.01)`
-            await self._busy_sig.wait()
+            self._serial_port.write(b"Q\n")
+            line = await self._serial_port.areadline()
+            self._busy = (line[0:1] != b"R")
+            # self.logger.debug(line[0:1])
+            self._serial_port.write(b"G\n")
+            self._position = float(await self._serial_port.areadline())
+            if self._busy:
+                await asyncio.sleep(0.1)
+            
+
